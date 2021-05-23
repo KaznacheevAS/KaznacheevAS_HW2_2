404ca51bad202a3fe9c7dd66ffd6a0874be2264f
Kaznacheev <antkaznacheev@gmail.ru>
2021-05-24 00:02:46 +0300

    Закгрузка проекта Урок 2

--- KaznacheevAS_HW2.2/ViewController.swift
+++ KaznacheevAS_HW2.2/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bagckgraundView: UIView!
+    
+    @IBOutlet weak var textRed: UILabel!
+    
+    
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.tintColor = UIColor.red
+        }
+    }
+    let brightRed = UIColor(displayP3Red: 1.0 / 255, green: 0.0 / 255, blue: 0.0 / 255, alpha: 1.0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bagckgraundView.layer.cornerRadius = 10
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 1
     }
 
-
+    @IBAction func tugleSlider() {
+        textRed.text = "Red \(String(format: "%.1f", slider.value))"
+    }
+    
+    
+    
+    
 }
 
